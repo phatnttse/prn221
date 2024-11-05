ccdbd57364e2eda268e6eaf6a2a9df358763e1f0
UBRO32BIT <90371372+UBRO32BIT@users.noreply.github.com>
2024-11-05 16:24:03 +0700

    update homepage

--- Blossom/Pages/Index.cshtml.cs
+++ Blossom/Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 using BusinessObjects.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Services.Interfaces;
 
 namespace Blossom.Pages
 {
@@ -11,18 +12,22 @@ namespace Blossom.Pages
         [BindProperty]
         public List<Category> CategoryList { get; set; }
 
+        private readonly IFlowerService _flowerService;
+        //private readonly ICategoryService _categoryService;
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IFlowerService flowerService)
         {
             _logger = logger;
+            _flowerService = flowerService;
             FlowerList = new();
             CategoryList = new();
         }
 
-        public void OnGet()
+        public async Task<IActionResult> OnGet()
         {
-
+            FlowerList = (await _flowerService.GetAllFlowersAsync()).ToList();
+            return Page();
         }
     }
 }

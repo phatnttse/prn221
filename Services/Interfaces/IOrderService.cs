15da11e0035e7b3483d34c55e7b46d347dae6f11
NGUYEN-VU\Lenovo <bwfnguyenvu@gmail.com>
2024-11-05 13:23:36 +0700

    [BWNGUYENVU][FEAT][ORDER SERVICE]

---
+++ Services/Interfaces/IOrderService.cs
@@ -0,0 +1,18 @@
+﻿using BusinessObjects.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Interfaces
+{
+    public interface IOrderService
+    {
+        public Task<IEnumerable<Order>> GetAllOrdersAsync();
+        public Task<Order> GetOrderByIdAsync(string id);
+        public Task AddOrderAsync(Order order);
+        public Task UpdateOrderAsync(Order order);
+        public Task RemoveOrderAsync(Order order);
+    }
+}
